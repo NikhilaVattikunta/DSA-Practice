6997c018ab9983134c72e7ca179f1c3bd1f171fb
NikhilaVattikunta <vattikuntans@gmail.com>
2026-02-04 23:47:14 +0530

    Create FrequencyMap.java

---
+++ FrequencyMap.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+
+public class FrequencyMap {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 3, 3, 4};
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        System.out.println("Element Frequencies:");
+        for (int key : map.keySet()) {
+            System.out.println(key + " -> " + map.get(key));
+        }
+    }
+}
